7b3a83218678dce8203c1d70cf211fbf9a6e9455
pedramnili <33115144@n>
2025-10-12 11:06:22 +0330

    change npm with pnpm for use workspace

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import "assets/indesx.css";
+import {Button} from "@repo/ui/button"
+
+
+function App() {
+  return (
+    <Button appName={"Test"}>
+      Hello
+    </Button>
+  )
+}
+
+export default App
---
+++ apps/web/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server : {
+    port : 2025,
+    host : true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000/",
+        changeOrigin: true,
+        secure: false
+      },
+    },
+  },
+})
